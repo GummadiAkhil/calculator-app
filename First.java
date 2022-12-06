45e822538e5660aa2505bbd64c0340bdb818fe87
akhil <gakhil238@gmail.com>
2022-12-06 13:44:42 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,8 @@
+class First{
+    public static void main(String[] args) {
+       int a = 5;
+       if( ++a>10 || ++a<15){
+            System.out.println(a);
+       }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+class Message{
+	public static void main(String[]args){
+		System.out.println("in Message class");
+	}
+}
+class Test{
+	public static void main(String[]args){
+		System.out.println("in Test class");
+	}
+}
